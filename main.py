d1566853f7a6397005a38c0f65f50dfdee80dce9
Elisabeth Everhart <eeverhart@wm.edu>
2025-11-28 12:26:47 -0500

    Changed the code to have six different activation patterns in response to six different height ranges

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ from light import light
 updating = False
 motorsrunning = False
 
+message1 = "message 1"
+message2 = "message 2"
+message3 = "message 3"
+message4 = "message 4"
+message5 = "message 5"
+message6 = "message 6"
+
 a1i = 17
 a2i = 27
 b1i = 22
@@ -51,16 +58,27 @@ def firstmotorclockwise(rotations, duration):
 def firstmotorcounterclockwise(rotations, duration):
     m.rotateccw(rotations, duration)
 
-def secondmotor(rotations, duration):
-    #mtwo.rotateccw(rotations, duration)
-    pass
+def secondmotorclockwise(rotations, duration):
+    mtwo.rotatecw(rotations, duration)
+
+def secondmotorcounterclockwise(rotations, duration):
+    mtwo.rotateccw(rotations, duration)
 
 def lights():
     l.lights()
 
+def red():
+    l.red()
+
+def green():
+    l.green()
+
+def blue():
+    l.blue()
+
 def closedistance():
     t1 = threading.Thread(target=firstmotorclockwise, args = (2, 4,))
-    t2 = threading.Thread(target=secondmotor, args = (8, 10,))
+    t2 = threading.Thread(target=secondmotorcouterclockwise, args = (8, 10,))
     t3 = threading.Thread(target=lights, args = ())
 
     t1.start()
@@ -71,7 +89,7 @@ def closedistance():
 
 def fardistance():
     t1 = threading.Thread(target=firstmotorcounterclockwise, args = (2, 4,))
-    t2 = threading.Thread(target=secondmotor, args = (8, 10,))
+    t2 = threading.Thread(target=secondmotorclockwise, args = (8, 10,))
     t3 = threading.Thread(target=lights, args = ())
 
     t1.start()
@@ -80,8 +98,91 @@ def fardistance():
     threading.Thread(target=wait_for_threads, args=(t1, t2, t3)).start()
     print('Done!')
 
-def printwords():
-    print("testing testing!")
+#Start the threads for running the two motors and the LEDs
+def activate(t1, t2, t3):
+    mindfulness_message.value = "Wheels are turning"
+    t1.start()
+    t2.start()
+    t3.start()
+    threading.Thread(target=wait_for_threads, args=(t1, t2, t3)).start()
+
+#How the wheel should behave when activated at the first height
+#The first motor (inner) will rotate 10 times counter clockwise
+#The second motor will rotate 5 times clockwise
+#The duration is 30 seconds
+#The LEDs will light up blue
+def behavior1():
+    t1 = threading.Thread(target=firstmotorcounterclockwise, args = (10, 30,))
+    t2 = threading.Thread(target=secondmotorclockwise, args = (5, 30,))
+    t3 = threading.Thread(target=blue, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message1
+
+#Second height
+#Inner motor 5 times counter clockwise
+#Outer motor 10 times clockwise
+#Duration 30 seconds
+#LEDs blue
+def behavior2():
+    t1 = threading.Thread(target=firstmotorcounterclockwise, args = (5, 30,))
+    t2 = threading.Thread(target=secondmotorclockwise, args = (10, 30,))
+    t3 = threading.Thread(target=blue, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message2
+
+#Third height
+#Inner motor 5 times clockwise
+#Outer motor 10 times counter clockwise
+#Duration 30 secdonds
+#LEDs green
+def behavior3():
+    t1 = threading.Thread(target=firstmotorclockwise, args = (5, 30,))
+    t2 = threading.Thread(target=secondmotorcounterclockwise, args = (10, 30,))
+    t3 = threading.Thread(target=green, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message3
+
+#Fourth height
+#Inner motor 10 times clockwise
+#Outer motor 5 times counter clockwise
+#Duration 30 seconds
+#LEDs green
+def behavior4():
+    t1 = threading.Thread(target=firstmotorclockwise, args = (10, 30,))
+    t2 = threading.Thread(target=secondmotorcounterclockwise, args = (5, 30,))
+    t3 = threading.Thread(target=green, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message4
+
+#Fifth height
+#Inner motor 10 times clockwise
+#Outer motor 5 times clockwise
+#Duration 30 seconds
+#LEDs red
+def behavior5():
+    t1 = threading.Thread(target=firstmotorclockwise, args = (10, 30,))
+    t2 = threading.Thread(target=secondmotorclockwise, args = (5, 30,))
+    t3 = threading.Thread(target=red, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message5
+
+#Sixth height
+#Inner motor 5 times counter clockwise
+#Outer motor 10 times counter clockwise
+#Duration 30 seconds
+#LEDs red
+def behavior6():
+    t1 = threading.Thread(target=firstmotorcounterclockwise, args = (5, 30,))
+    t2 = threading.Thread(target=secondmotorcounterclockwise, args = (10, 30,))
+    t3 = threading.Thread(target=red, args = ())
+
+    activate(t1, t2, t3)
+    mindfulness_message.value = message6
 
 def getdistance():
     GPIO.output(trigger, 1)
@@ -106,19 +207,38 @@ def getdistance():
 def updatedistance():
     if updating:
         distance = getdistance()
-        distance_text.value = f"Distance: {distance} m"
         
         global motorsrunning
         if(not motorsrunning):
-            if(distance < 0.25):
-                print(f"close distance: {distance:.1f} m")
+            if(distance < 0.147):
+                print("behavior 1")
                 motorsrunning = True
-                closedistance()
+                behavior1()
 
-            elif(distance < 0.5):
-                print(f"medium distance: {distance:.1f} m")
+            elif(distance < 0.194):
+                print("behavior 2")
                 motorsrunning = True
-                fardistance()
+                behavior2()
+            
+            elif(distance < 0.241):
+                print("behavior 3")
+                motorsrunning = True
+                behavior3()
+
+            elif(distance < 0.288):
+                print("behavior 4")
+                motorsrunning = True
+                behavior4()
+
+            elif(distance < 0.335):
+                print("behavior 5")
+                motorsrunning = True
+                behavior5()
+
+            elif(distance < 0.382):
+                print("behavior 6")
+                motorsrunning = True
+                behavior6()
 
         app.after(500, updatedistance)
 
@@ -140,9 +260,9 @@ def startupdating():
     updatedistance()
 
 app = App(title="Everspin")
-distance_text = Text(app, text="Distance: -- cm", size=20)
 button = PushButton(app, text="start", command=startupdating)
 button = PushButton(app, text="stop", command=stopupdating)
+mindfulness_message = Text(app, text="Welcome to Everspin", size=20)
 app.when_closed = lambda: GPIO.cleanup()
 app.display()
 
