72927a5de387f53bf8264a790e0901c820972016
Elisabeth Everhart <eeverhart@wm.edu>
2025-12-01 15:07:44 -0500

    Added comments to the main.py file

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import threading
 from motor import motor
 from light import light
 
-#This file will be the main program for the mindfullness wheel. It will launch the GUI, control the motors, and control the microphone
+#This file is the main program for the mindfullness wheel.
+#It launches a GUI and then controls the movement of the motors and lights based on a distance sensor
 
 #Flags
 updating = False
@@ -41,7 +42,7 @@ l = light(red, green, blue)
 m = motor(a1i, a2i, b1i, b2i)
 mtwo = motor(a1o, a2o, b1o, b2o)
 
-trigger = 16 #hopefully this doesn't cause problems
+trigger = 16
 GPIO.setup(trigger, GPIO.OUT)
 
 echo = 21
@@ -51,6 +52,9 @@ GPIO.output(trigger, 0)
 time.sleep(2)
 
 soundspeed = 343
+
+#A series of functions that then call functions from the light and motor classes
+#This allows for more easy interfacing with threading
 
 def firstmotorclockwise(rotations, duration):
     m.rotatecw(rotations,duration)
@@ -76,29 +80,7 @@ def green():
 def blue():
     l.blue()
 
-def closedistance():
-    t1 = threading.Thread(target=firstmotorclockwise, args = (2, 4,))
-    t2 = threading.Thread(target=secondmotorcounterclockwise, args = (8, 10,))
-    t3 = threading.Thread(target=lights, args = ())
-
-    t1.start()
-    t2.start()
-    t3.start()
-    threading.Thread(target=wait_for_threads, args=(t1, t2, t3)).start()
-    print('Done!')
-
-def fardistance():
-    t1 = threading.Thread(target=firstmotorcounterclockwise, args = (2, 4,))
-    t2 = threading.Thread(target=secondmotorclockwise, args = (8, 10,))
-    t3 = threading.Thread(target=lights, args = ())
-
-    t1.start()
-    t2.start()
-    t3.start()
-    threading.Thread(target=wait_for_threads, args=(t1, t2, t3)).start()
-    print('Done!')
-
-#Start the threads for running the two motors and the LEDs
+#Start the threads for running the two motors and the LEDs and calls the wait_for_threads function
 def activate(t1, t2, t3, message):
     mindfulness_message.value = "Wheels are turning"
     t1.start()
@@ -178,6 +160,7 @@ def behavior6():
 
     activate(t1, t2, t3, message6)
 
+#Function to trigger the distance sensor and convert the time to distance based on the speed of sound
 def getdistance():
     GPIO.output(trigger, 1)
     for i in range(180):
@@ -198,6 +181,8 @@ def getdistance():
     distance = soundspeed*duration/2
     return distance
 
+#Calls the getdistance function and determines if and how to activate the wheel
+#Function loops using app.after to update every 500ms
 def updatedistance():
     if updating:
         distance = getdistance()
@@ -236,7 +221,7 @@ def updatedistance():
 
         app.after(500, updatedistance)
 
-
+#Manages the timing of the threads and updates the mindfulness message and lights after the wheel is done spinning
 def wait_for_threads(t1, t2, t3, message):
     t1.join()
     t2.join()
@@ -246,25 +231,26 @@ def wait_for_threads(t1, t2, t3, message):
     mindfulness_message.value = message
     l.off()
 
+#Stops the program from triggering the wheel based on the distance sensor. Distance is still measured
 def stopupdating():
     global updating
     updating = False
 
+#Starts the programs response to the distance sensor
 def startupdating():
     global updating
     updating = True
     updatedistance()
 
+#Runs GPIO.cleanup() and ends the program
 def do_this_when_closed():
     GPIO.cleanup()
     app.destroy()
 
-
+#Main code for the GUI of the program
 app = App(title="Everspin")
 button = PushButton(app, text="start", command=startupdating)
 button = PushButton(app, text="stop", command=stopupdating)
 mindfulness_message = Text(app, text="Welcome to Everspin", size=20)
 app.when_closed = do_this_when_closed
 app.display()
-
-#TODO need to update the GUI to have the picture and then have two separate buttons one to start and one to end the session and export the CSV
